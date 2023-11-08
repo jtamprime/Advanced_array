5e6715b4fd446e2acf270276c3cecadfc873022e
Jeffrey <jtamprime@gmail.com>
2023-11-07 23:25:12 -0500

    problem2

--- problems/01-avg-val-forEach.js
+++ problems/01-avg-val-forEach.js
@@ -15,7 +15,20 @@ console.log(avgVal([])); // null
 
 let avgVal = function(arr) {
     // Your code here
+    if (arr.length == 0) {
+      return null
+    }
+    let total = 0;
+    arr.forEach(function(num){
+      total += num
+    })
+    let avg = total/arr.length;
+    return avg
 };
+
+console.log(avgVal([5, 10])); // 7.5
+console.log(avgVal([3, 7, 2, 1, 2])); // 3
+console.log(avgVal([])); // null
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
