06fc1ceff23d78ebe318385dfbca61545ca62433
Jeffrey <jtamprime@gmail.com>
2023-11-08 22:02:12 -0500

    problem5&6

--- problems/04-remove-e-words-filter.js
+++ problems/04-remove-e-words-filter.js
@@ -14,12 +14,23 @@ console.log(removeEWords('Enter the building')); // 'building'
 
 let removeEWords = function(sentence) {
     // Your code here
+    let array = sentence.split(' ');
+    let newSentence = array.filter(function(word){
+      if (!word.includes('e')){
+        return word;
+      }
+
+    });
+  return newSentence.join(' ');
 };
 
+
+console.log(removeEWords('What time is it everyone?')); // 'What is it'
+console.log(removeEWords('Enter the building')); // 'building'
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
   module.exports = removeEWords;
 } catch (e) {
   module.exports = null;
-}
+}
--- problems/05-choosey-endings-filter.js
+++ problems/05-choosey-endings-filter.js
@@ -27,12 +27,39 @@ console.log(chooseyEndings(17, 'ily'));
 
 let chooseyEndings = function(words, suffix) {
   // Your code here
+  if (words.length == 0) {
+    return [];
+  };
+  if (!Array.isArray(words)) {
+    return [];
+  };
+  let newArray = words.filter(function(word){
+    if (word.endsWith(suffix)){
+      return word;
+    }
+  });
+  return newArray; 
 };
 
+
+console.log(chooseyEndings(['family', 'hound', 'catalyst', 'fly', 'timidly', 'bond'], 'ly'));
+// [ 'family', 'fly', 'timidly' ]
+
+console.log(chooseyEndings(['family', 'hound', 'catalyst', 'fly', 'timidly', 'bond'], 'nd'));
+// [ 'hound', 'bond' ]
+
+console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'icity'));
+// [ 'simplicity', 'felicity' ]
+
+console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'ily'));
+// [ ]
+
+console.log(chooseyEndings(17, 'ily'));
+// [ ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
   module.exports = chooseyEndings;
 } catch (e) {
   module.exports = null;
-}
+}
