92396a7db5702f2c7a444a09e8f7f96ed68ba208
Jeffrey <jtamprime@gmail.com>
2023-11-08 21:42:19 -0500

    problem3&4

--- problems/02-tripler-map.js
+++ problems/02-tripler-map.js
@@ -13,8 +13,13 @@ console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
 
 let tripler = function(nums) {
     // Your code here
+    let newArr = nums.map(function(num){
+      return num*3
+    });
+    return newArr;
 };
-
+console.log(tripler([2, 7, 4])); // [ 6, 21, 12 ]
+console.log(tripler([-5, 10, 0, 11])); // [ -15, 30, 0, 33 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
--- problems/03-long-words-filter.js
+++ problems/03-long-words-filter.js
@@ -15,12 +15,19 @@ console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // 
 
 let longWords = function(words) {
     // Your code here
+let newArr= words.filter(function(word){
+   if (word.length > 5){
+    return word;
+   }
+});
+return newArr;
 };
-
+console.log(longWords(['bike', 'skateboard','scooter', 'moped'])); // [ 'skateboard', 'scooter' ]
+console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // [ 'couscous', 'ceviche', 'solyanka' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
   module.exports = longWords;
 } catch (e) {
   module.exports = null;
-}
+}
